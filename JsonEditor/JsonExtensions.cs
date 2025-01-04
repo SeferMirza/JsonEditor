6c806b0627c4c698375054c2ce9f5f9c7ef38237
Sefer Mirza <sefermirza@gmail.com>
2025-01-05 00:33:00 +0300

    initialize project

---
+++ JsonEditor/JsonExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace JsonEditor;
+
+public static class JsonExtensions
+{
+    public static Dictionary<string, object> ParseToDictionary(this JsonElement element)
+    {
+        var dictionary = new Dictionary<string, object>();
+
+        foreach (var property in element.EnumerateObject())
+        {
+            if (property.Value.ValueKind == JsonValueKind.Object)
+            {
+                dictionary[property.Name] = ParseToDictionary(property.Value);
+            }
+            else if (property.Value.ValueKind == JsonValueKind.Array)
+            {
+                var list = new List<object>();
+                foreach (var item in property.Value.EnumerateArray())
+                {
+                    list.Add(item.ValueKind == JsonValueKind.Object ? ParseToDictionary(item) : item.GetRawText());
+                }
+                dictionary[property.Name] = list;
+            }
+            else
+            {
+                dictionary[property.Name] = property.Value.GetRawText();
+            }
+        }
+
+        return dictionary;
+    }
+}
---
+++ JsonEditor/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JsonEditor;
+
+public partial class MainWindow : Window
+{
+    Dictionary<string, object> _jsonData = [];
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Refresh()
+    {
+        
+    }
+
+    private void OpenNewJsonButtonClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new Microsoft.Win32.OpenFileDialog();
+        dialog.FileName = "Document";
+        dialog.DefaultExt = ".json";
+        dialog.Filter = "Json files (.json)|*.json";
+        dialog.Multiselect = false;
+
+        bool? result = dialog.ShowDialog();
+
+        if (result == true)
+        {
+            string filename = dialog.FileName;
+
+            string caption = "Json file path";
+            MessageBoxButton button = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult mboxResult;
+            mboxResult = MessageBox.Show($"Is file path '{filename}' correct?", caption, button, icon, MessageBoxResult.Yes);
+            if (mboxResult == MessageBoxResult.Yes)
+            {
+                using StreamReader r = new(filename);
+                string jsonString = r.ReadToEnd();
+
+                using var document = JsonDocument.Parse(jsonString);
+                var element = document.RootElement.ParseToDictionary();
+
+                _jsonData = element;
+            }
+        }
+
+        addNewPropertyToModel.IsEnabled = true;
+    }
+
+    private void AddNewPropertyToModel(object sender, RoutedEventArgs e)
+    {
+    }
+}
